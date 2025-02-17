51a1e6fe76ca776be55d8f452857459ce8ce36ca
hzkm613 <hzkm613@gmail.com>
2025-02-16 20:09:31 -0500

    Create a user register form using React Hook Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import './App.css'
 import { createGlobalStyle } from 'styled-components';
+import ToDoList from './ToDoList';
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -70,6 +70,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   )
 }

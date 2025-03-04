3bbb109323c3e801f2cec1165704aa31ae2603fc
hzkm613 <hzkm613@gmail.com>
2025-03-03 19:29:33 -0500

    Update To Do categories as enum

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,28 +1,29 @@
 import { useSetRecoilState } from "recoil";
-import { IToDo, toDoState } from "./atoms";
+import { Categories, IToDo, toDoState } from "./atoms";
 import React from "react";
 
-const ToDo = ({ text, category, id }: IToDo) => {
-
+function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
-
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
-
     setToDos((oldToDos) => {
-      const targetIndex: number = oldToDos.findIndex((toDo: IToDo) => toDo.id === id);
-      const newToDo = {text,id , category: name as 'TO_DO' | 'DOING' | 'DONE'};
-      return [...oldToDos.slice(0, targetIndex), newToDo, ...oldToDos.slice(targetIndex + 1)];
-    });  
-  }
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
 
   return <li>
     <span>{text}</span> 
-    {category !== "DOING" && (<button name="DOING" onClick={onClick}>To Do</button>)} 
-    {category !== "TO_DO" && (<button name="TO_DO" onClick={onClick}>Doing</button>)} 
-    {category !== "DONE" && (<button name="DONE" onClick={onClick}>Done</button>)}    
+    {category !== Categories.DOING && (<button name={Categories.DOING + ""} onClick={onClick}>Doing</button>)} 
+    {category !== Categories.TO_DO && (<button name={Categories.TO_DO + ""} onClick={onClick}>To Do</button>)} 
+    {category !== Categories.DONE && (<button name={Categories.DONE + ""} onClick={onClick}>Done</button>)}    
     </li>;
 }
 
